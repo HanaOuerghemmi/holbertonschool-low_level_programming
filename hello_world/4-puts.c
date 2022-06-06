ee6eaa036acff94f5357eacad64fd30c96d9258f
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-06 20:34:36 +0100

    function puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * prints exactly Programming is like building a multilingual puzzle, followed by a new line using the function puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts ("\"Programming is like building a multilingual puzzle\n");
+    	return (0);
 }
