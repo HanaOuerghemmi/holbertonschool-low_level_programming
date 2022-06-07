cf29b5f2060345777534e4f61378e3d5de4981d1
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-07 09:24:06 +0100

    print text without printf and puts

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 64);
+fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 64);
 return (1);
 }
