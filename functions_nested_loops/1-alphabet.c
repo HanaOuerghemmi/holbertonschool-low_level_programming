6f014a623779a3bf52b573f2c93526127d0443c7
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-08 20:29:32 +0100

    print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - header file print alphabet
+ *
+ * Return: Void
+ */
+
+void print_alphabet(void)
+{
+
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
+
