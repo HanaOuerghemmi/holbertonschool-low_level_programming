291fa1d2249ea41363989305b2ba8e8aade2fa8b
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-08 20:52:37 +0100

    print alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main -print alphabet 10 time
+ *
+ * Return void
+ */
+
+void print_alphabet_x10(void);
+{
+	char a;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		i++;
+	}
+
+
+}
+
