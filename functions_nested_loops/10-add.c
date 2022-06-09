c7e7cd4c95034058a8a0a55fb9ac45efd73c78d8
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-09 10:37:21 +0100

    add tow integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - print the last digit.
+ *
+ * @a: number to be cheked
+ * @b: number to be cheked
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
