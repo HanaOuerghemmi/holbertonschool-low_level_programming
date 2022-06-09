25e47d61ecdfd4f99ec2d9c62cb60cf99f6666c5
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-09 08:04:42 +0100

    is alpha

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * * _islower - checks if the caracter lowercase
+ * _islower - checks if the caracter lowercase
  * @c: The character to be checked.
  *
  * Return : 1 if character is lowercase , 0otherwise.
