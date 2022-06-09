b22a8583c21b02fb5906a6932f550377d6610f98
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-09 08:30:37 +0100

    is absolute

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - check the code.
+ *
+ * @n: number to be cheked
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
+
+
