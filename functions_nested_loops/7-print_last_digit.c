be67bad805ea73257386ebab0b05b31895d40ef7
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-09 09:06:56 +0100

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - check the code.
+ *
+ * @n: number to be cheked
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	_putchar(n % 10);
+	return (n % 10);
+}
+
