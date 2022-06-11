21bb4779b89ce26eaf45e69204c584012c545475
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-12 00:55:51 +0100

    multipilies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - checks for  digit (0 through 9).
+ *
+ * @a: is the varible to check it
+ * @b: the variable to check
+ * Return: 1 if digit else 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
