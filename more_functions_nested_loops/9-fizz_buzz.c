2da24b501daec93e72aab4f195989466d447b9ed
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-12 04:09:03 +0100

    fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,9 @@ int main(void)
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
 		else if ((i % 3 == 0))
-			printf("FIZZ");
+			printf("Fizz");
 		else if ((i % 5 == 0))
-			printf("buzz");
+			printf("Buzz");
 		else
 			printf("%d", i);
 		if (i != 100)
