bc0b482f45d0294774205574459dee2aa7f05d39
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-14 13:49:23 +0100

    point string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - print a string in reverse
+ *
+ *@str: variable return the  string.
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] % 2) == 0)
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
