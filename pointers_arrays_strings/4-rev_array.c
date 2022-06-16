82c92be520c290213f75d0b72434cfcfd30f7438
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-16 09:45:15 +0100

    reverse array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - print a string in reverse
+ *
+ *@a: array
+ *@n: size
+ * Return: void.
+ */
+void reverse_array(int *a, int n)
+{
+	int aux[n], i;
+
+	for (i = 0; i < n; i++)
+	{
+		aux[n - 1 - i] = a[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = aux[i];
+	}
+}
