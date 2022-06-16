20996f35a204c5f00541a749ac072bdfea9cbbd7
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-16 13:04:18 +0100

    copie memory area

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - function that copie memory erea
+ *
+ * @dest: pointeur
+ * @src: thz source 
+ * @n: bytes of the memory
+ * Return: void
+ */
+void *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src [i]
+	return (dest);
+
+}
+
