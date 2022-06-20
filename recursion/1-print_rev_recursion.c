2c03211019eef501f243ec91c2e8099265bd2105
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-20 19:29:47 +0100

    print reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that print a string
+ *
+ * @s: pointeur string
+ * Return: Void.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
+
