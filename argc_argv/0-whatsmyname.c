83bc0a8cf141238c78cd42cb5d71e2012befbbb0
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-23 10:31:31 +0100

    whatsmyname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - program that print its name followed by new line
+ * mainp- program that print its name followed by new line
  *
  * @argc: argc
  * @argv: argv
- * Return: name
+ * Return: void
  */
-int main(int argc, char **argv)
+void mainp(int argc, char **argv)
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
+
 }
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,1 +1,2 @@
 int _putchar(char c);
+void mainp(int argc, char **argv);
