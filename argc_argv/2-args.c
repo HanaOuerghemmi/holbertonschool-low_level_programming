e594e4cf2fd432146d9b1ea5504b8a64a6f14539
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-23 11:10:40 +0100

    number of arg

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- program that print its name followed by new line
+ *
+ * @argc: argc
+ * @argv: argv
+ * Return: void
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+
+}
