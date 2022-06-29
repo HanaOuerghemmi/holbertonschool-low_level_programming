5f811dffbbcf90b2a71e5f38ca6a06512309dab7
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-29 09:08:11 +0100

    define structur dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H
 #define _DOG_H
-/* this struct define a dog*/
+/**
+ * struct dog - this struct define a dog
+ * @name: dog name type char*
+ * @age: dog age type float*
+ * @owner: the owner type char*
+ */
+
 struct dog
 {
 char *name;
