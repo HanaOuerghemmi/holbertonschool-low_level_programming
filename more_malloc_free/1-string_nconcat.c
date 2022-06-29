910a9c3635d44053790ffac3731912eac97e3a54
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-29 20:24:15 +0100

    string n conact with malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,27 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *a;
-	unsigned int i, size;
-	unsigned int ls1, ls2;
-
-	ls1 = strlen(s1);
-	ls2 = strlen(s2);
-
-	/*If n is greater or equal to the length of s2*/
-	 /*then use the entire string s2 */
-
-	if (n >= ls2)
-		size = ls1 + ls2;
-	else
-		size = ls1 + n;
-
-	/* aloocate memory*/
-
-	a = malloc(sizeof(char) * size);
-
-	/* If the function fails, it should return NULL*/
-	if (a == NULL)
-		return (NULL);
+	unsigned int i, len = n;
 
 	/*if NULL is passed, treat it as an empty string*/
 
@@ -41,12 +21,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	/*concat */
 
-	for (i = 0; i < ls1; i++)
-		a[i] = s1[i];
-	for (i = 0; i < n; i++)
-		a[i + ls1] = s2[i];
+	/* count the size to allocate */
+	for (i = 0; s1[i]; i++)
+		len++;
+	/* aloocate memory*/
+
+	a = malloc(sizeof(char) * len + 1);
+
+	/* If the function fails, it should return NULL*/
+	if (a == NULL)
+		return (NULL);
+
+	/*concat */
+	len = 0;
+	for (i = 0; s1[i]; i++)
+		a[len++] = s1[i];
+	for (i = 0; s2[i] && i < n; i++)
+		a[len++] = s2[i];
+	a[len] = '\0';
 
 	return (a);
 
