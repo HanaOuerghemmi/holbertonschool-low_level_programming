5d4d05ed3493acfe6d4db0e4953e8c7c4b264a0c
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-29 19:53:56 +0100

    string n conact with malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
+#include<string.h>
 #include <stdlib.h>
 /**
  * string_nconcat - function that concatenates tow string
@@ -13,23 +13,35 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *a;
-	unsigned long i;
-	int size;
+	unsigned int i, size;
+	unsigned int ls1, ls2;
+
+	ls1 = strlen(s1);
+	ls2 = strlen(s2);
+
+	/*If n is greater or equal to the length of s2*/
+	 /*then use the entire string s2 */
+
+	if (n >= ls2)
+		size = ls1 + ls2;
+	else
+		size = ls1 + n;
+
+	/* aloocate memory*/
+
+	a = malloc(sizeof(char) * size);
+
+	/* If the function fails, it should return NULL*/
+	if (a == NULL)
+		return (NULL);
+
+	/*if NULL is passed, treat it as an empty string*/
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	if (n >= (strlen(s2)))
-		size = strlen(s1) + strlen(s2);
-	else
-		size = strlen(s1) + n;
-
-	a = malloc(size + 1);
-
-	if (a == NULL)
-		return (NULL);
+	/*concat */
 
 	for (i = 0; i < (strlen(s1)); i++)
 		a[i] = s1[i];
