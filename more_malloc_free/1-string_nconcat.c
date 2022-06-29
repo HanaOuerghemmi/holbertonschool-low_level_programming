865ef333d953f01651544ea75740a1f329650c6e
HanaOuerghemmi <5004@holbertonschool.com>
2022-06-29 19:30:03 +0100

    string n conact with malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,23 +14,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *a;
 	unsigned long i;
-	int size = strlen(s1);
+	int size;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	if (n >= (strlen(s2)))
+		size = strlen(s1) + strlen(s2);
+	else
+		size = strlen(s1) + n;
 
-	a = malloc(size + n);
+	a = malloc(size + 1);
 
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (strlen(s1)); i++)
 		a[i] = s1[i];
 	for (i = 0; i < n; i++)
-		a[i + size + 1] = s2[i];
+		a[i + strlen(s1)] = s2[i];
 
 	return (a);
 
