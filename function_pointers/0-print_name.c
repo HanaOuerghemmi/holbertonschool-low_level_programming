6018c3898f2e9f0b23c84bcbe01e3f07e22995ec
HanaOuerghemmi <5004@holbertonschool.com>
2022-07-01 18:56:58 +0100

    print name with function pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 /**
  * print_name - function that print name
@@ -7,5 +8,8 @@
  */
 void print_name(char *name, void(*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
