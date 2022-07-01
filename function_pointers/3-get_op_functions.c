cae6f07b13562d42b29416922e4edc752b4c62eb
HanaOuerghemmi <5004@holbertonschool.com>
2022-07-01 21:45:22 +0100

    simple operations

---
+++ function_pointers/3-get_op_functions.c
@@ -0,0 +1,25 @@
+#include "calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * get_op_func- function add a + b
+ * @s: the aargument
+ * Return: int
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != *s)
+		i++;
+	return(op[i].f);
+
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include "calc.h"
+#include<stdlib.h>
+/**
+ * main - a program that performs simple operations
+ * @argc: argiment count
+ * @argv: argument value
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int a, b, r;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+			&& argv[2][0] != '/' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+
+	}
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	r = f(a, b);
+	printf("%d\n", r);
+	return (0);
+}
