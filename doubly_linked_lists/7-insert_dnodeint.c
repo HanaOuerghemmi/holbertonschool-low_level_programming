706d36e26e7dd0ea6772d19017dd637f00d0d533
HanaOuerghemmi <5004@holbertonschool.com>
2022-07-20 22:03:21 +0100

    insert double list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->prev = tmp;
 	new->next = tmp->next;
-	tmp->prev = new;
+	tmp->next->prev = new;
 	tmp->next = new;
 
 
